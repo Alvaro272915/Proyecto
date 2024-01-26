3600497cea6e786fafa6b6b43873ecdd2880aadb
Alvaro272915 <alvarourregoviana@gmail.com>
2024-01-25 20:48:41 -0500

    Segundo Ejercisio JS

--- Ejercicios/Modulo1.js
+++ Ejercicios/Modulo1.js
@@ -1,7 +1,6 @@
 
 
 // Realizar el algoritmo que convierta litros en centilitros, decilitros, galones y onzas
-
 let litros = 1 ;
 let centilitros = 100 ;
 let decilitros = 10 ;
@@ -26,4 +25,21 @@ resultado = litros * onzas ;
 console.log('Litros en Centilitros : ' + resultado) ;
 
 
+
+//Realizar el algoritmo que realice las 4 operaciones básicas para dos números de entrada.
+let operador1 = parseInt(prompt("Ingrese el Primero Operador")) ;
+let operador2 = parseInt(prompt("Ingrese el Segundo Operador")) ;
+
+// Multiplicacion
+resultado = operador1 * operador2
+console.log('Operación Multiplicacion : ' + resultado) ;
+// Suma
+resultado = (operador1 + operador2)
+console.log('Operación Suma : ' + resultado) ;
+// Resta
+resultado = operador1 - operador2
+console.log('Operación Resta : ' + resultado) ;
+// Resta
+resultado = operador1 / operador2
+console.log('Operación Dicisión : ' + resultado) ;
 
