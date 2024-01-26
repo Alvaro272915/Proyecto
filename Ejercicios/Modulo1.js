0e5c823963896b356faafc49482ca3d12680b257
Alvaro272915 <alvarourregoviana@gmail.com>
2024-01-25 20:08:07 -0500

    Cargue de Ejercicio1 JS

---
+++ Ejercicios/Modulo1.js
@@ -0,0 +1,29 @@
+
+
+// Realizar el algoritmo que convierta litros en centilitros, decilitros, galones y onzas
+
+let litros = 1 ;
+let centilitros = 100 ;
+let decilitros = 10 ;
+let galones = 0.219969 ;
+let onzas = 33.814 ;
+let resultado = 0 ; 
+
+// Convertir Litros en Centilitros
+resultado = litros * centilitros;
+console.log('Litros en Centilitros : ' + resultado) ;
+
+// Convertir Litros en Decilitros
+resultado = litros * decilitros ;
+console.log('Litros en Decilitros : ' + resultado) ;
+
+// Convertir Litros en Galones
+resultado = litros * galones ;
+console.log('Litros en Galones : ' + resultado) ;
+
+// Convertir Litros en Onzas
+resultado = litros * onzas ;
+console.log('Litros en Centilitros : ' + resultado) ;
+
+
+
